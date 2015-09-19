fd3e01864cca7abb73d720600bcf1a5e2056c41d
stratos <karlstratos@gmail.com>
2015-09-19 15:33:11 -0400

    .

--- context_free_grammar/scripts/nbest-trees.py
+++ context_free_grammar/scripts/nbest-trees.py
@@ -1,29 +1,41 @@
 # Author: Karl Stratos (stratos@cs.columbia.edu)
 """
-This module is used to extract n-best parses from sentences.
+This module is used to extract n-best parses from trees.
 """
 import argparse
 from bllipparser import RerankingParser
+from bllipparser.RerankingParser import Tree
 
-def extract_nbest_from_sentences(args):
-    """Extracts n-best parses from each sentence in a file."""
+def extract_nbest_from_trees(args):
+    """Extracts n-best parses from each tree in a file."""
     parser = RerankingParser.fetch_and_load(args.parser, verbose=False)
     parser.set_parser_options(nbest=args.n)
     with open(args.output_path, "w") as outfile:
-        with open(args.sentences_path, "r") as infile:
+        with open(args.trees_path, "r") as infile:
             for line in infile:
                 if not line.strip(): continue
-                nbest = parser.parse(line, rerank=True)
+                gold_tree = Tree.trees_from_string(line)[0]
+                gold_tree.label = args.label
+                gold_tree_string = str(gold_tree)
+                outfile.write(str(gold_tree) + "\n");
+                sentence = " ".join(gold_tree.tokens())
+                nbest = parser.parse(sentence, rerank=True)
+                num_trees = 1  # Already have the gold tree.
                 for item in nbest:
+                    if num_trees >= args.n: break
                     tree = item.ptb_parse
                     tree.label = args.label
-                    outfile.write(str(tree) + "\n")
+                    tree_string = str(tree)
+
+                    if tree_string != gold_tree_string:
+                        num_trees += 1
+                        outfile.write(tree_string + "\n")
                 outfile.write("\n")
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("sentences_path", type=str, help="path to file with "
-                           "a sentence per line")
+    argparser.add_argument("trees_path", type=str, help="path to file with "
+                           "a tree per line")
     argparser.add_argument("output_path", type=str, help="path to output")
     argparser.add_argument("--parser", type=str, default="WSJ+Gigaword-v2",
                            help="parser model: WSJ-PTB3, WSJ+Gigaword-v2, "
@@ -35,4 +47,4 @@ if __name__ == "__main__":
                            help="label for the top node of a tree "
                            "(default: %(default)s)")
     parsed_args = argparser.parse_args()
-    extract_nbest_from_sentences(parsed_args)
+    extract_nbest_from_trees(parsed_args)
