532326788713d2f5e4048ebb79bb5dfdadb3e91e
stratos <karlstratos@gmail.com>
2015-09-19 15:58:15 -0400

    .

--- context_free_grammar/scripts/nbest-trees.py
+++ context_free_grammar/scripts/nbest-trees.py
@@ -1,6 +1,7 @@
 # Author: Karl Stratos (stratos@cs.columbia.edu)
 """
-This module is used to extract n-best parses from trees.
+This module is used to extract n-best parses from trees. Note that we only use
+(n-1) trees from the parser if the gold tree is already in the list.
 """
 import argparse
 from bllipparser import RerankingParser
@@ -12,6 +13,9 @@ def extract_nbest_from_trees(args):
     parser.set_parser_options(nbest=args.n)
     with open(args.output_path, "w") as outfile:
         with open(args.trees_path, "r") as infile:
+            num_instances = 0
+            num_instances_with_gold = 0
+            avg_gold_rank = 0
             for line in infile:
                 if not line.strip(): continue
                 gold_tree = Tree.trees_from_string(line)[0]
@@ -21,7 +25,8 @@ def extract_nbest_from_trees(args):
                 sentence = " ".join(gold_tree.tokens())
                 nbest = parser.parse(sentence, rerank=True)
                 num_trees = 1  # Already have the gold tree.
-                for item in nbest:
+                nbest_has_gold = False
+                for i, item in enumerate(nbest):
                     if num_trees >= args.n: break
                     tree = item.ptb_parse
                     tree.label = args.label
@@ -30,7 +35,25 @@ def extract_nbest_from_trees(args):
                     if tree_string != gold_tree_string:
                         num_trees += 1
                         outfile.write(tree_string + "\n")
+                    else:
+                        nbest_has_gold = True
+                        avg_gold_rank += i+1
+
                 outfile.write("\n")
+                num_instances += 1
+                if nbest_has_gold: num_instances_with_gold += 1
+
+            if not args.quiet:
+                print "{0} out of {1} ({2:.1f}%) had gold tree in " \
+                    "{3}-best list".format(num_instances_with_gold,
+                                           num_instances,
+                                           float(num_instances_with_gold) *
+                                           100.0 / num_instances, args.n)
+                if num_instances_with_gold:
+                    print "In those {0} instances, the average rank of " \
+                        "the gold tree was {1:.1f}".format(
+                        num_instances_with_gold, float(avg_gold_rank) /
+                        num_instances_with_gold)
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
@@ -46,5 +69,6 @@ if __name__ == "__main__":
     argparser.add_argument("--label", type=str, default="TOP",
                            help="label for the top node of a tree "
                            "(default: %(default)s)")
+    argparser.add_argument("--quiet", action="store_true", help="no messages")
     parsed_args = argparser.parse_args()
     extract_nbest_from_trees(parsed_args)
